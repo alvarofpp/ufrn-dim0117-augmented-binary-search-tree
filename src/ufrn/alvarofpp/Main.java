843933a1131af70b3b8681359120b0e07248fec6
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-05-20 16:48:49 -0300

    Arquivos para testes

--- src/ufrn/alvarofpp/Main.java
+++ src/ufrn/alvarofpp/Main.java
@@ -1,20 +1,37 @@
 package ufrn.alvarofpp;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Valores que usaremos para gerar a árvore
-        // int valores[] = {5, 11, 12, 14, 15, 18, 19, 21, 23, 25, 27, 28, 30, 32, 37};
-        int valores[] = {50, 30, 70, 20, 40, 60, 80, 15, 25, 35, 45, 36};
         // Objeto para chamarmos os comandos da árvore
         BinarySearchTree bst = new BinarySearchTree();
         // Nó que conterá a raiz
         NodeBST node = null, nodeOther;
-        // Gerando a árvore
-        for(int valor : valores) {
-            nodeOther = new NodeBST(valor);
-            node = bst.insercao(node, nodeOther);
+        // Valores que usaremos para gerar a árvore
+        BufferedReader buffer = null;
+
+        try {
+            buffer = new BufferedReader(new FileReader("/home/roleta/IdeaProjects/edb2-trabalho-abb/src/ufrn/alvarofpp/input/input1.txt"));
+
+            int valores[] = Arrays.asList(buffer.readLine().split(" "))
+                    .stream()
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            // Gerando a árvore
+            for(int valor : valores) {
+                nodeOther = new NodeBST(valor);
+                node = bst.insercao(node, nodeOther);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
 
         System.out.print("(");
         bst.toString(node);
